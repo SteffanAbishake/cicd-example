e3b6edcb8d775eccb0c0870ab3aaf70d1a696d64
“[Steffan <steffanabishake1234@gmail.com>
2023-02-01 14:04:21 +0530

    first

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+//	echo "# cicd-example" >> README.md
+//	git init
+	//git status
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/SteffanAbishake/cicd-example.git
+//	git push -u origin main
+
+}
